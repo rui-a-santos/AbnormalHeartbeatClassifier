f5c9dbda3187208183b15bb4ce3df26160cd2ddc
ruipsantos <44174970+ruipsantos@users.noreply.github.com>
2021-10-18 15:32:27 +0100

    Update comments

--- AbnormalHeartbeatClassifier.py
+++ AbnormalHeartbeatClassifier.py
@@ -2,8 +2,8 @@
 """AbnormalHeartbeatsClassifier.ipynb
 
 Authors: Rui , Matt , Louis. 
-This is intented to run in online google colab. There are sections that make it easier to navigate. 
-The dataset in the folder is downloaded from https://www.kaggle.com/shayanfazeli/heartbeat and uploaded to google colab as 'heartbeat.zip'. 
+This py file is downloaded from google colab. There are sections that make it easier to navigate. For easier execution of code, use .ipynb file.
+The dataset is downloaded from https://www.kaggle.com/shayanfazeli/heartbeat and uploaded to google colab as 'heartbeat.zip'. 
 In order to run each model with different datasets, you will need to change the X_input variables before training each model. 
 In order to recreate the training process use seed(1). Datasets need to prepared before training and this may involve a 
 non-linear running of the colab code snippets for some cases in transfer-learning and NR+IN runs.
